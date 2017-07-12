beaaddf9929954be4298ac32ecfff416d7b85f31
Zachary Levy <zacharyalevy@gmail.com>
2017-07-11 22:54:22 -0400

    docs

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id         :integer          not null, primary key
+#  body       :jsonb
+#  source     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :feedback do
     body ""
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id         :integer          not null, primary key
+#  body       :jsonb
+#  source     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
