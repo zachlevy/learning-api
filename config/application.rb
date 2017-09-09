0f228dd2d74761e3a1723e9db6688a214cbfbfec
Zach Levy <zacharyalevy@gmail.com>
2017-09-09 19:15:29 -0400

    sendgrid mailer

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,26 @@ module LearningApi
           :methods => [:get, :post, :options, :delete, :put]
       end
     end
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'learning-api-production.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+
+    config.after_initialize do
+      Rails.application.routes.default_url_options = {
+        :host => "localhost",
+        :port => "3000"
+      }
+    end
+
+
   end
 end
