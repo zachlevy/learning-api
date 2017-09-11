913603e6a61e93ff0c1ffde26dcd98f74551b620
Zach Levy <zacharyalevy@gmail.com>
2017-09-11 17:23:19 -0400

    add user to challenge response

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 8..20 }
+
+  has_many :challenge_responses
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910180205) do
+ActiveRecord::Schema.define(version: 20170911212005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20170910180205) do
     t.bigint "challenge_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["challenge_id"], name: "index_challenge_responses_on_challenge_id"
+    t.index ["user_id"], name: "index_challenge_responses_on_user_id"
   end
 
   create_table "challenge_types", force: :cascade do |t|
@@ -101,5 +103,6 @@ ActiveRecord::Schema.define(version: 20170910180205) do
   end
 
   add_foreign_key "challenge_responses", "challenges"
+  add_foreign_key "challenge_responses", "users"
   add_foreign_key "challenges", "challenge_types"
 end
