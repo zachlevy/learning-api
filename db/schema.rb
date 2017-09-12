4567322e4d6356c817a3f43429a2e61667ded140
Zachary Levy <zacharyalevy@gmail.com>
2017-09-12 12:23:59 -0400

    user to profile migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911212005) do
+ActiveRecord::Schema.define(version: 20170912162249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 20170911212005) do
     t.jsonb "details", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -105,4 +107,5 @@ ActiveRecord::Schema.define(version: 20170911212005) do
   add_foreign_key "challenge_responses", "challenges"
   add_foreign_key "challenge_responses", "users"
   add_foreign_key "challenges", "challenge_types"
+  add_foreign_key "profiles", "users"
 end
