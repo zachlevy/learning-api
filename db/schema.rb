7406b428e87645ed5b9b5b45b2f66454b1208411
Zachary Levy <zacharyalevy@gmail.com>
2017-09-14 15:35:00 -0400

    default false admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914192949) do
+ActiveRecord::Schema.define(version: 20170914193423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20170914192949) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "challenge_responses", "challenges"
