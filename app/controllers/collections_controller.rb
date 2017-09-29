9370257ff707a906e0c547ed35fcb068376075fb
Zachary Levy <zacharyalevy@gmail.com>
2017-09-29 15:36:50 -0400

    controller slug

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -41,7 +41,7 @@ class CollectionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_collection
-      @collection = Collection.find(params[:id])
+      @collection = Collection.friendly.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
